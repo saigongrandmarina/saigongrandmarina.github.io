2b8597a620ae0507ec85e859d33abf46475f1644
saigongrandmarina <oanhbn7@gmail.com>
2025-07-14 00:58:13 +0700

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -24,6 +24,8 @@ if (empty($_POST['_gotcha'])) { // If hidden field was filled out (by spambots) 
     // Create the email and send the message
     //$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
     $to = 'oanhbn7@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+    $email_action = "https://formspree.io/f/xdkdngvj";
+    $email_method = "POST"
     $email_subject = "Website Contact Form:  $name";
     $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
     $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
